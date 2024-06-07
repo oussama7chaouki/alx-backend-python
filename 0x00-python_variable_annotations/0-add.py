3dbe62726f0086a1c5d1d4ca36c2841adb65a050
Oussama Chaouki <oussama11chaouki@gmail.com>
2024-06-07 21:39:54 +0000

    add

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -2,7 +2,8 @@
 '''Task 0: Basic annotations - add
 '''
 
+
 def add(a: float, b: float) -> float:
     '''Adds two floating-point numbers.
     '''
-    return a + b
+    return a + b`
--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -2,6 +2,7 @@
 '''Task 1: Basic annotations - concat
 '''
 
+
 def concat(str1: str, str2: str) -> str:
     '''Concatenates two strings.
     '''
